c2f301221c425f6cbe9d227c5745d2b7f302611f
Emily Ng <emilyng318@gmail.com>
2024-07-30 13:15:23 -0400

    Initial Commit

---
+++ TravelBuddy/PersonalityTestPage.swift
@@ -0,0 +1,18 @@
+//
+//  PersonalityTest.swift
+//  TravelBuddy
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct PersonalityTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PersonalityTest()
+}
---
+++ TravelBuddy/TravelBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelBuddyApp.swift
+//  TravelBuddy
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
